6fdce1f420620691abbf9d0eae195f140a2bf8db
Gaby Fisher <gabrielleyfisher@gmail.com>
2026-05-17 14:54:01 -0400

    chore: implement environment variables for API routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,8 +9,8 @@ interface Todo {
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [input, setInput] = useState('');
-  // Make sure to keep your actual live Render URL here
-  const BACKEND_URL = 'https://todo-fullstack-gabyfisher.onrender.com/todos';
+  
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
   useEffect(() => {
     fetchTodos();
